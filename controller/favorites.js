ba0da10628c19459d317295f185cea6045e634fc
ellekim89 <ellekim89@gmail.com>
2015-08-11 18:34:11 -0700

    adding tags

--- controller/favorites.js
+++ controller/favorites.js
@@ -5,7 +5,7 @@ var db = require("../models");
 
 router.get('/', function(req, res){
   db.favorite.findAll({
-    include:[db.comment]
+    include:[db.comment, db.tag]
   }).then(function(favorites){
       res.render('favorites/index', {myFavorites: favorites
                                     });
@@ -32,13 +32,15 @@ router.get('/:id/comments', function(req, res){
   // res.render('comments/index', {favoriteId: req.params.id});
   db.favorite.find({
     where:{id:req.params.id},
-    include: [db.comment]
+    include: [db.comment, db.tag]
   }).then(function(favorite){
     res.render('comments/index',{
       favorite:favorite
     })
+  // res.send(favorite)
   })
 });
+
 
 router.post('/:id/comments', function(req,res){
   db.favorite.findById(req.params.id).then(function(favorite){
@@ -46,6 +48,40 @@ router.post('/:id/comments', function(req,res){
       res.redirect('/favorites/' + favorite.id + '/comments');
     });
   });
+})
+
+router.get('/:id/tags', function(req, res) {
+  db.tag.find({
+    where: {id: parseInt(req.params.id)},
+    include: [db.favorite]
+  }).then(function(tag) {
+    if (tag) {
+      res.render('tags/show', {tag: tag});
+    } else {
+      res.send("There are no movies related to this tag");
+    }
+  }).catch(function(error) {
+    res.send("There are no movies related to this tag");
+  });
+});
+
+
+router.get('/:id/tags/new', function(req, res){
+  res.render('tags/index', {favoriteId: req.params.id});
+
+});
+
+router.post('/:id/tags', function(req, res){
+  var tagName = req.body.tags;
+  var favoriteId = req.params.id;
+
+  db.favorite.findById(favoriteId).then(function(fave){
+    db.tag.findOrCreate({where: {name: tagName}}).spread(function(tag, created){
+      fave.addTag(tag).then(function(){
+        res.redirect('/favorites/' + favoriteId + '/comments')
+      })
+    })
+  })
 })
 
 
